6f22c570a60c1e1d440e35ea9e087c55f39280e7
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2023-09-15 11:09:10 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ sr.reveal('form', {origin: 'bottom'});
 
 // typadd.js
 var typed = new Typed('.typing', {
-    strings: ['Programmer', 'Web Developer', 'Data Scientist', 'ML Engineer', 'DL Engineer'],
+    strings: ['Programmer', 'Web Developer', 'Data Scientist', 'ML Engineer', 'DL Engineer', 'FreeLancer'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
