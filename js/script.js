4487c2dd13a20b5ab01f856f689b511862a6a0b8
Ilyas Hidayat Rusdy <saylihidayatrusdy@gmail.com>
2024-11-26 15:10:53 +0700

    Update: animation and send message to email

--- js/script.js
+++ js/script.js
@@ -32,6 +32,15 @@ window.onscroll = () => {
     nav.classList.remove('active');
 };
 
+const button = document.getElementById('toggleButton');
+const description = document.getElementById('description');
+
+button.addEventListener('click', function(event) {
+    event.preventDefault(); // Mencegah link dari melakukan navigasi
+    description.classList.toggle('max-desc'); // Mengganti class max-desc
+    button.textContent = description.classList.contains('max-desc') ? 'Read More' : 'Read Less'; // Mengubah teks button
+});
+
 
 // ScrollReveal Animation
 const sr = ScrollReveal({
@@ -56,6 +65,28 @@ sr.reveal('.timeline', {origin: 'bottom'});
 sr.reveal('.timeline-box', {origin: 'bottom'});
 sr.reveal('form', {origin: 'bottom'});
 
+sr.reveal('.skills-box', {
+    beforeReveal: (el) => {
+        const progressBar = el.querySelector('.progress-bar'); // Ambil elemen progress-bar
+        const targetWidth = progressBar.getAttribute('data-target'); // Ambil target dari atribut data
+        animateProgressBar(progressBar, targetWidth); // Mulai animasi progress bar
+    }
+});
+
+// Fungsi untuk animasi progress bar
+function animateProgressBar(progressBar, targetWidth) {
+    let width = 0;
+
+    const interval = setInterval(() => {
+        if (width >= targetWidth) {
+            clearInterval(interval);
+        } else {
+            width++;
+            progressBar.style.width = width + '%';
+        }
+    }, 20); // Sesuaikan kecepatan animasi di sini
+}
+
 // typadd.js
 var typed = new Typed('.typing', {
     strings: ['Programmer', 'Web Developer', 'Data Scientist', 'ML Engineer', 'DL Engineer', 'FreeLancer'],
@@ -68,7 +99,7 @@ var typed = new Typed('.typing', {
 // contact form
 function sendWhastapp() {
     // phone number
-    let number = '+6285883415570';
+    let number = '+62#############';
 
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
@@ -76,9 +107,44 @@ function sendWhastapp() {
     let subject = document.getElementById('subject').value;
     let message = document.getElementById('message').value;
 
+     // Validasi input
+     if (!name || !email || !phone || !subject || !message) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
     let url = 'https://wa.me/' + number + '?text=' + 'Name: ' + name + '%0AEmail: ' + email + '%0APhone: ' + phone + '%0ASubject: ' + subject + '%0AMessage: ' + message;
 
     window.open(url, '_blank').focus();
+}
+
+function sendEmail() {
+    // email penerima
+    let emailRecipient = 'saylihidayatrusdy@gmail.com';  // Ganti dengan email tujuan
+ 
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    let subject = document.getElementById('subject').value;
+    let message = document.getElementById('message').value;
+ 
+    // Validasi input
+    if (!name || !email || !phone || !subject || !message) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    // Membuat URL mailto dengan parameter untuk subjek dan body
+    let url = 'mailto:' + emailRecipient + 
+              '?subject=' + encodeURIComponent('Subject: ' + subject) + 
+              '&body=' + encodeURIComponent('Name: ' + name + 
+                                             '%0AEmail: ' + email + 
+                                             '%0APhone: ' + phone + 
+                                             '%0ASubject: ' + subject + 
+                                             '%0AMessage: ' + message);
+ 
+    // Membuka aplikasi email dengan URL mailto
+    window.location.href = url;
 }
 
 // form.addEventListener('submit', (e) => {
